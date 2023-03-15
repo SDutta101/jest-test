1be2a17ec883a9f03e34724e67cc6592397cb40e
SauravD11 <119333044+SauravD11@users.noreply.github.com>
2023-03-15 14:06:21 +0530

    Update css.test.js

--- src/tests/css.test.js
+++ src/tests/css.test.js
@@ -1,31 +1,26 @@
+// Description:
+// Verify that the h1 tag gas blue color.
+
 const puppeteer = require('puppeteer');
 
-describe("App.js", () => {
-  let browser;
-  let page;
+let browser;
 
-  const width = 1440;
-  const height = 700;
-
-  beforeEach(async () => {
+beforeAll(async () => {
     browser = await puppeteer.launch({
-      headless: true,
-      slowMo: 25,
-      args: [`--window-size=${width},${height}`],
-      defaultViewport: {
-        width,
-        height,
-      },
-    });
-    page = await browser.newPage();
-    await page.goto('http://localhost:3000');
-  }, 100000);
+        executablePath: process.env.CHROMIUM_PATH,
+        args: ['--no-sandbox'], // This was important. Can't remember why
+      });
+});
 
-  it("h1 tag gas blue color", async () => {
-    const heading = await page.$('h1');
-    const h1ComputedStyle = await page.evaluate(el => window.getComputedStyle(el).color, heading);
-    expect(h1ComputedStyle).toBe('rgb(0, 0, 255)');
-  });
+afterAll(async () => {
+  await browser.close();
+});
 
-  afterAll(() => browser.close());
+test('h1 tag gas blue color', async () => {
+  const page = await browser.newPage();
+  await page.goto('http://localhost:8080');
+  
+  const heading = await page.$('h1');
+  const h1ComputedStyle = await page.evaluate(el => window.getComputedStyle(el).color, heading);
+  expect(h1ComputedStyle).toBe('rgb(0, 0, 255)');
 });
